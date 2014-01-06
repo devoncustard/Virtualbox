6657736ab4470bb0a1ff3cf1033364b4c08508e1
devoncustard <joe@mojo-online.net>
2014-01-06 10:47:55 +0000

    updates

--- host scripts/buildme.cmd
+++ host scripts/buildme.cmd
@@ -1,2 +1,2 @@
 
-powershell -ExecutionPolicy Unrestricted -File c:\dropbox\scripts\sysprep\prov_vagrant_box.ps1 -boxname wintest -boxos windows -hostname %1 -domain devops.local -puppet puppet.devops.local -environment joe -user vagrant -password vagrant -classes mcollective_deploy,sysbuild
+powershell -ExecutionPolicy Unrestricted -File prov_vbox.ps1 -boxname wintest -boxos windows -hostname %1 -domain devops.local -puppet puppet.devops.local -environment joe -user vagrant -password vagrant -classes mcollective_deploy,sysbuild
